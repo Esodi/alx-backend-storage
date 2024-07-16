13567fe01a350e08729ed214eb91c490b12dfc0b
Method Mkoma <gershommethod02@gmail.com>
2024-07-16 18:22:56 +0300

    task 8

--- 0x01-NoSQL/8-all.py
+++ 0x01-NoSQL/8-all.py
@@ -1,8 +1,7 @@
 #!/usr/bin/env python3
 ''' lists all documents in a collection '''
 
-if __name__ == "__main__":
-    def list_all(mongo_collection):
-        ''' function itself '''
-        docs = list(mongo_collection.find())
-        return docs if docs else []
+def list_all(mongo_collection):
+    ''' function itself '''
+    docs = list(mongo_collection.find())
+    return docs if docs else []
