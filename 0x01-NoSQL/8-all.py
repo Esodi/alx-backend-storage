228893801618bcf448f65817afdaf186d0864055
Method Mkoma <gershommethod02@gmail.com>
2024-07-16 18:14:40 +0300

    task 8

---
+++ 0x01-NoSQL/8-all.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# lists all documents in a collection
+
+def list_all(mongo_collection):
+    if not mongo_collection:
+        return []
+
+    return list(mongo_collection.find())
